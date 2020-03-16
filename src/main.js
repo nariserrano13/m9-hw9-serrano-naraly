4dd9b61fdc5263b7a4a95bbad02aa36038cba0f1
Naraly Serrano <naralycserrano@ufl.edu>
2020-03-16 01:12:44 -0400

    Write classes to main.js

---
+++ src/main.js
@@ -0,0 +1,27 @@
+class Desserts {
+    constructor(type, calories){
+        this.type= type;
+        this.calories=calories;
+        console.log('test');
+    }
+}
+
+class IceCream extends Desserts{
+    constructor (type, calories, flavor, scoops){
+        super(type, calories);
+        this.flavor=flavor;
+        this.scoops=scoops; 
+    }
+
+    includeSpoon(){
+        if(this.type==='Ice Cream'){
+            console.log('Here is your spoon!');
+        }
+    }
+
+}
+
+const vanillaIceCream = new IceCream ('Ice Cream', 340,'Vanilla',3)
+
+console.log(vanillaIceCream);
+vanillaIceCream.includeSpoon();
